6df2928074ad63e329e23f3b2a408e51aa30f1b7
bobbyZ94 <r.zioltkowski@gmail.com>
2023-03-03 10:33:11 +0100

    feat: added masonry layout to showcase

--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -2,17 +2,17 @@
 	// @ts-nocheck
 	import type { PageData } from './$types'
 	export let data: PageData
-	const { showcase } = data
+	const showcase = data.showcase.sort((itemA, itemB) => new Date(itemB.date) - new Date(itemA.date))
 	import Masonry from 'svelte-bricks'
 
-	let [minColWidth, maxColWidth, gap] = [200, 800, 20]
-	let width: number
-	let height: number
+	let [minColWidth, maxColWidth, gap]: [minColWith: number | undefined, maxColWidth: number | undefined, gap: number | undefined] = [500, 1200, 30]
+	let masonryWidth: number | undefined
+	let masonryHeight: number | undefined
 </script>
 
-Masonry size: <span>{width}px</span> &times; <span>{height}px</span> (w &times; h)
-
-<Masonry {showcase} {minColWidth} {maxColWidth} {gap} let:item bind:width bind:height>
-	<!-- <img src={showcase.image.url} alt={showcase.image.title} /> -->
-	<div class="h-20 w-20 bg-green-500" />
-</Masonry>
+<div class="my-10 px-5 sm:my-20 sm:px-20">
+	<Masonry items={showcase} {minColWidth} {maxColWidth} {gap} let:item bind:masonryWidth bind:masonryHeight>
+		<button />
+		<img src={item.image.url} alt={item.image.title} class="object-cover drop-shadow-lg duration-200 ease-in-out hover:scale-105" />
+	</Masonry>
+</div>
