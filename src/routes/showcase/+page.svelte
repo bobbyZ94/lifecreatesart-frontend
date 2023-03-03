2087e436582e9aa2c3d93b80c1f44f74c8a01ce2
bobbyZ94 <r.zioltkowski@gmail.com>
2023-03-03 17:28:05 +0100

    fix: bug fixed with masonry

--- src/routes/showcase/+page.svelte
+++ src/routes/showcase/+page.svelte
@@ -5,14 +5,14 @@
 	const showcase = data.showcase.sort((itemA, itemB) => new Date(itemB.date) - new Date(itemA.date))
 	import Masonry from 'svelte-bricks'
 
-	let [minColWidth, maxColWidth, gap]: [minColWith: number | undefined, maxColWidth: number | undefined, gap: number | undefined] = [500, 1200, 30]
+	let [minColWidth, maxColWidth, gap]: [minColWith: number | undefined, maxColWidth: number | undefined, gap: number | undefined] = [400, 1260, 30]
 	let masonryWidth: number | undefined
 	let masonryHeight: number | undefined
 </script>
 
-<div class="my-10 px-5 sm:my-20 sm:px-20">
+<div class="my-10 max-w-[100rem] px-5 sm:my-20 sm:px-20">
 	<Masonry items={showcase} {minColWidth} {maxColWidth} {gap} let:item bind:masonryWidth bind:masonryHeight>
 		<button />
-		<img src={item.image.url} alt={item.image.title} class="object-cover drop-shadow-lg duration-200 ease-in-out hover:scale-105" />
+		<img src={item.image.url} alt={item.image.title} class="m-0 object-cover p-0 drop-shadow-lg duration-200 ease-in-out hover:scale-105" />
 	</Masonry>
 </div>
